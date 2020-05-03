fb81bb692a7772d06cf335e88bd2d72491cdbf96
DinozaurG <49711433+DinozaurG@users.noreply.github.com>
2020-05-03 18:58:17 +0700

    Initial Commit

---
+++ AppSwoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppSwoosh
+//
+//  Created by Алексей Шумейко on 03.05.2020.
+//  Copyright © 2020 Алексей Шумейко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
